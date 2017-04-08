71a2017c20cc8c0f412b6026514cda4d563bdfb7
aikaterna <aikaterna@hotmail.com>
2017-04-07 21:43:31 -0700

    Add chatterbot.py

---
+++ chatterbot/chatterbot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+try:
+    from chatterbot import ChatBot
+    chatbotInstalled = True
+except:
+    chatbotInstalled = False
+
+class Chatterbot:
+    """Chatter with the bot."""
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(pass_context=True)
+    async def chatterbot(self, ctx, message):
+        """Talk with the bot via ChatterBot."""
+        chatbot = ChatBot('Red-DiscordBot', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+        chatbot.train("chatterbot.corpus.english")
+        await self.bot.say(chatbot.get_response(message))
+
+def setup(bot):
+    if chatbotInstalled is False:
+        raise RuntimeError("Install ChatterBot:\n"
+                           "[p]debug bot.pip_install('ChatterBot')\n"
+                           "Then [p]load chatterbot")
+    bot.add_cog(Chatterbot(bot))
