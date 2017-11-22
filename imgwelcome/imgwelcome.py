25ca6efe2b7506f3372094f129f25f981bd99f12
Sitryk <jamesfitzy1937@gmail.com>
2017-11-22 17:07:18 +1300

    server name length thing
    Enhancement
    
    Long server names will no longer ride off of the end of the image. They will be shortened with '...'

--- imgwelcome/imgwelcome.py
+++ imgwelcome/imgwelcome.py
@@ -162,10 +162,12 @@ class ImgWelcome:
             members = test_member_number
 
         member_number = str(members) + self._get_suffix(members)
+        sname = str(member.server.name) + '!' if len(str(member.server.name)) <= 28 else str(member.server.name)[:23] + '...'
+
         _outline((152, 96), "You are the " + str(member_number) + " member", 1, server_font, (textoutline))
         drawtwo.text((152, 96), "You are the " + str(member_number) + " member", font=server_font, fill=(servercolor))
-        _outline((152, 116), "of " + str(member.server.name) + "!", 1, server_font, (textoutline))
-        drawtwo.text((152, 116), "of " + str(member.server.name) + "!", font=server_font, fill=(servercolor))
+        _outline((152, 116), 'of ' + sname, 1, server_font, (textoutline))
+        drawtwo.text((152, 116), 'of ' + sname, font=server_font, fill=(servercolor))
 
         image_object = BytesIO()
         welcome_picture.save(image_object, format="PNG")
