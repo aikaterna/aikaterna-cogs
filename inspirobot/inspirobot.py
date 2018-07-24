6c0c302125ffdd32233e35a2d1673568098e2d1f
aikaterna <20862007+aikaterna@users.noreply.github.com>
2018-07-24 10:36:56 -0700

    [V3 Inspirobot] Initial commit

---
+++ inspirobot/inspirobot.py
@@ -0,0 +1,24 @@
+import aiohttp
+import discord
+from redbot.core import commands
+
+class Inspirobot:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
+
+    @commands.command()
+    async def inspireme(self, ctx):
+        """Fetch a random "inspirational message" from the bot."""
+        try:
+            async with self.session.request("GET", "http://inspirobot.me/api?generate=true") as page:
+                pic = await page.text(encoding='utf-8')
+                em = discord.Embed()
+                em.set_image(url=pic)
+                await ctx.send(embed=em)
+        except Exception as e:
+                await ctx.send(f"Oops, there was a problem: {e}")
+
+    def __unload(self):
+        self.session.close()
