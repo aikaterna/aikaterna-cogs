a7fb88bad2feded8e850d531a312b4179d750925
aikaterna <20862007+aikaterna@users.noreply.github.com>
2019-06-25 14:20:24 -0700

    [CardsAgainstHumanity] No bundled_data in init

--- cah/__init__.py
+++ cah/__init__.py
@@ -1,8 +1,5 @@
-from redbot.core import commands, data_manager
 from .cah import CardsAgainstHumanity
 
 
-def setup(bot: commands.Bot):
-    n = CardsAgainstHumanity(bot)
-    data_manager.load_bundled_data(n, __file__)
-    bot.add_cog(n)
+def setup(bot):
+    bot.add_cog(CardsAgainstHumanity(bot))
