80e5095ebc2976d48d55d2cea70e380f68be89e7
aikaterna <20862007+aikaterna@users.noreply.github.com>
2019-10-04 19:14:47 -0700

    [RndStatus] Fix for not updating status

--- rndstatus/__init__.py
+++ rndstatus/__init__.py
@@ -2,6 +2,4 @@ from .rndstatus import RndStatus
 
 
 def setup(bot):
-    n = RndStatus(bot)
-    bot.add_listener(n.switch_status, "on_message")
-    bot.add_cog(n)
+    bot.add_cog(RndStatus(bot))
