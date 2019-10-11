a1ca7dff6035ba5afbc877de00b1698d16517648
PredaaA <46051820+PredaaA@users.noreply.github.com>
2019-10-11 03:36:15 +0200

    [TrickOrTreat] Fix humanize_number for Red < 3.2 (#74)
    * Update trickortreat.py
    
    * Update trickortreat.py

--- trickortreat/trickortreat.py
+++ trickortreat/trickortreat.py
@@ -3,11 +3,16 @@ import datetime
 import discord
 import random
 import math
+from typing import Union  # <- Remove this at 3.2
+from babel.numbers import format_decimal  # <- Remove this at 3.2
 from redbot.core import commands, checks, Config, bank
-from redbot.core.utils.chat_formatting import box, pagify, humanize_number
+from redbot.core.utils.chat_formatting import box, pagify  #, humanize_number <- Will be for 3.2
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
 __version__ = "0.0.6a"
+
+def humanize_number(val: Union[int, float]):  # <- Remove this at 3.2
+    return format_decimal(val, locale="en_US")
 
 
 class TrickOrTreat(commands.Cog):
