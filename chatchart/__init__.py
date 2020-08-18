9ccc3f13ed7a4b6038da71bc95a15b4ffda5fcda
Dav <dav@mail.stopdavabuse.de>
2020-08-18 14:47:14 +0000

    pull EUD statement from info.json

--- chatchart/__init__.py
+++ chatchart/__init__.py
@@ -1,5 +1,9 @@
 from .chatchart import Chatchart
+from pathlib import Path
+import json
 
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 def setup(bot):
     bot.add_cog(Chatchart(bot))
