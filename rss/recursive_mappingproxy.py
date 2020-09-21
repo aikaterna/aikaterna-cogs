d2810fa569867a822aff05531344a3503456d3f3
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-09-21 19:41:29 +0200

    Welp, looks like MappingProxyType isn't subclassable, that's a bummer

--- rss/recursive_mappingproxy.py
+++ rss/recursive_mappingproxy.py
@@ -1,5 +1,4 @@
 from collections.abc import Mapping, Sequence
-from types import MappingProxyType
 
 
 def _proxy(item):
@@ -14,17 +13,22 @@ def _proxied_sequence(value: list) -> tuple:
     return tuple(_proxy(item) for item in value)
 
 
-class RecursiveMappingProxyType(MappingProxyType):
+class RecursiveMappingProxyType(Mapping):
     """
     Like MappingProxyType, but it also recursively proxies values
     that are Mappings (dicts) or Sequences (lists, tuples).
     """
+    def __init__(self, data: dict):
+        self.__data = data
 
     def __getitem__(self, key):
-        return _proxy(super().__getitem__(key))
+        return _proxy(self.__data[key])
 
-    def get(self, key, default=None):
-        try:
-            return self.__getitem__(key)
-        except KeyError:
-            return default
+    def __len__(self):
+        return len(self.__data)
+
+    def __iter__(self):
+        return iter(self.__data)
+
+    def __contains__(self, key):
+        return key in self.__data
