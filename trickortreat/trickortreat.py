51ce9c98b690cc87361ae6f7d8fc9a7adae5cdd5
Alex <58824393+vertyco@users.noreply.github.com>
2022-10-06 19:11:36 -0400

    Fix typo for candy_type "chocolate"
    Candy type cannot be plural chocolate, results in KeyError when checking user candy count

--- trickortreat/trickortreat.py
+++ trickortreat/trickortreat.py
@@ -84,7 +84,7 @@ class TrickOrTreat(commands.Cog):
         if candy_type in ["stars", "star"]:
             candy_type = "stars"
         if candy_type in ["chocolate", "chocolates"]:
-            candy_type = "chocolates"
+            candy_type = "chocolate"
         if candy_type in ["cookie", "cookies"]:
             candy_type = "cookies"
         candy_list = ["candies", "chocolates", "lollipops", "cookies", "stars"]
