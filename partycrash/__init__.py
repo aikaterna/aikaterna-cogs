8efc4e68d1b213547ec485da008748e11cb23fc1
Honkertonken <94032937+Honkertonken@users.noreply.github.com>
2023-05-11 06:17:42 +0530

    [Partycrash] Async setup (#313)

--- partycrash/__init__.py
+++ partycrash/__init__.py
@@ -3,5 +3,5 @@ from .partycrash import PartyCrash
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 
-def setup(bot):
-    bot.add_cog(PartyCrash(bot))
+async def setup(bot):
+    await bot.add_cog(PartyCrash(bot))
